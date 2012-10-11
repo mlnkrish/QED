035ce1dccd3b9e54422249614e34d08291785520
mln <mln@linuxmint.linuxmint-domain>
2012-10-11 10:56:24 +0530

    MLN | rails 3 with bootstarp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qed::Application.config.secret_token = 'a86d43a3aba515917f34ec8764a17924794fb075134119b6cd6aa22b8b98b4db57c1c8d47ada2b269742bb1e7f39c6c4e514cde13f7e2aaef28b42279e1fa218'
