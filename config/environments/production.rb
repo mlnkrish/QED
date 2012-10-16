4f13ba44993bdf7f8f86b807ab5ee3ee915c1555
mln <mln@linuxmint.linuxmint-domain>
2012-10-16 17:51:34 +0530

    Moving links to buttons.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Qed::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  config.redis_host = "127.0.0.1"
+  config.redis_port = 6379
+  config.redis_db = 0
 end
